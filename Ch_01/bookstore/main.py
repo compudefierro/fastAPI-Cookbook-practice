1795a6a4be79ab89c643d6489354141e26ef99b6
compudefierro <jorgeeldri@gmail.com>
2025-07-17 14:24:12 -0300

    intro parameters year

--- Ch_01/bookstore/main.py
+++ Ch_01/bookstore/main.py
@@ -9,3 +9,19 @@ async def read_book(book_id: int):
         "title": "The Great Gatsby",
         "Autor": "F. Scott Fitzgerald"    
             }
+
+@app.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {
+        "author_id": author_id,
+        "name": "Ernest Hemingway"
+    }
+
+@app.get("/books")
+async def read_books(year: int= None):
+    if year:
+        return {
+            "year": year,
+            "books": ["Book 1", "Book 2"]
+        }
+    return {"books": ["All Books]"]} 
